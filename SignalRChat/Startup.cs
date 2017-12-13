437b7b7b82b6dcfec7243a8660b6b7d1b9762903
FrankFrankWei <yanzhifei25@163.com>
2017-12-13 21:54:35 +0800

    first check in

---
+++ SignalRChat/Startup.cs
@@ -0,0 +1,25 @@
+﻿/******************************************************************
+** auth: Frank
+** date: 12/10/2017 5:50:17 PM
+** desc:
+******************************************************************/
+
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(SignalRChat.Startup))]
+namespace SignalRChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
---
+++ WebChatWithSignalR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebChatWithSignalR.Startup))]
+namespace WebChatWithSignalR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
